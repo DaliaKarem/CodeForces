da5ab7a82b6e0ae082dd5245cbf5c5fd247dee20
Dalia Karem <daliakarem0027@gmail.com>
2021-12-05 22:32:51 +0200

    A - Watermelon

--- CodeForces/main.cpp
+++ CodeForces/main.cpp
@@ -4,20 +4,13 @@ using namespace std;
 
 int main()
 {
-    int n;
-    string w;
-    cin >> n;
-    while (n--)
-    {
-        cin >> w;
-         if (w.length() > 10)
-        {
-            cout << w[0] << w.length() - 2 << w[w.length() - 1] << endl;
-        }
-        else
-        {
-            cout << w<< endl;
-        }
-    }
+   int w;
+   cin>>w;
+   if(w%2==0&& w>2){
+    cout<<"YES"<<endl;
+   }
+   else{
+    cout<<"NO"<<endl;
+   }
     return 0;
 }
